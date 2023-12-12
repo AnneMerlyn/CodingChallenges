b3c18a8540f1df90ac3cdae9b8dbd940930daa44
Anne Merlyn Martinez <martinezannemerlyn@gmail.com>
2023-12-12 17:50:11 +0100

    updated

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -8,6 +8,7 @@ namespace CodingChallenges {
         static void Main(string[] args)
         {
             //Coding Challenge : Work out the number of Days
+            Console.WriteLine("Coding Challenge : Work out the number of Days");
             DateTime fromDate = DateTime.Now;
             DateTime toDate = new DateTime(2023, 12, 19);
 
@@ -15,12 +16,14 @@ namespace CodingChallenges {
             Console.WriteLine($"Days left: {daysLeft}");
 
             //Coding Challenge: string removal
+            Console.WriteLine($"{Environment.NewLine}Coding Challenge: string removal");
             string value = "Hello";
             string result = RemoveLastCharacterFunctions.RemoveLastCharacter(value);
             Console.WriteLine("Current String: {0}", value);
             Console.WriteLine("Result of the string from Removal: {0}", result);
 
             //Convert MHP to KPH
+            Console.WriteLine($"{Environment.NewLine}Convert MHP to KPH");
             decimal speed = 100;
             decimal convertedSpeed = SpeedHelper.ConvertSpeed(speed, SpeedTypeEnum.KPH);
 
